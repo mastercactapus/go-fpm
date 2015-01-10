bd885b55dc92f26dbd567785d6ed1233ba2f2b9c
Nathan Caza <mastercactapus@gmail.com>
2015-01-10 14:47:17 -0600

    more semver compare work

--- semver.go
+++ semver.go
@@ -6,12 +6,42 @@ import (
 	"strings"
 )
 
-type SemverRequirements struct {
-	requirements []requirementSet
+const (
+	svLT = iota
+	svLTE
+	svGT
+	svGTE
+	svEQ
+)
+
+type requirement struct {
+	version semver.Version
+	svType  int
 }
 
-type requirementSet []requirementFn
-type requirementFn func(semver.Version) bool
+type SemverRequirements struct {
+	requirements [][]requirement
+}
+
+//strips any prefix for the version for parsing
+func stripPrefix(version string) string {
+	switch version[0] {
+	case '>', '<':
+		if version[1] == '=' {
+			version = version[2:]
+		} else {
+			version = version[1:]
+		}
+	case '=', '^', '~':
+		version = version[1:]
+	}
+
+	if version[0] == 'v' {
+		version = version[1:]
+	}
+
+	return version
+}
 
 //parses, replacing missing/wildcards with zero
 func parseDown(version string) (sv semver.Version, err error) {
@@ -35,8 +65,12 @@ func parseDown(version string) (sv semver.Version, err error) {
 	if parts[2] == "x" || parts[2] == "X" || parts[2] == "*" {
 		parts[2] = "0"
 	}
-	return semver.Parse(strings.Join(parts, "."))
-
+	version = strings.Join(parts, ".")
+	buildIndex := strings.IndexRune(version, '+')
+	if buildIndex != -1 {
+		version = version[:buildIndex]
+	}
+	return semver.Parse(version)
 }
 
 //parses, replacing missing/wildcards by incrementing the higher version
@@ -70,105 +104,118 @@ func parseUp(version string) (sv semver.Version, round bool, err error) {
 	return
 }
 
-// func parseHyphenRange(version string) {
+//checks, if a prerelease, that the major,minor,patch tuple match
+func validPrerelease(orig semver.Version, sv semver.Version) bool {
+	if sv.Pre == nil {
+		return true
+	}
+	if orig.Pre != nil {
+		return false
+	}
+	return orig.Major == sv.Major && orig.Minor == sv.Minor && orig.Patch == sv.Patch
+}
 
-// }
+func checkVersion(req requirement, sv semver.Version) bool {
+	switch req.svType {
+	case svLT:
+		return sv.LT(req.version)
+	case svLTE:
+		return sv.LTE(req.version)
+	case svGT:
+		return sv.GT(req.version)
+	case svGTE:
+		return sv.GTE(req.version)
+	case svEQ:
+		return sv.EQ(req.version)
+	default:
+		panic("Unknown comparator")
+	}
+}
 
 func NewSemverRequirements(requirements string) (*SemverRequirements, error) {
 	sr := new(SemverRequirements)
 
 	//cleanup, trim and remove duplicate whitespace
 	requirements = strings.TrimSpace(requirements)
-	requirements = strings.Replace(requirements, "  ", " ", -1)
 	parts := strings.Split(requirements, " ")
-	sr.requirements = make(requirementSet, 0, len(parts))
-	var currentReq []requirementFn
-
+	sr.requirements = make([][]requirement, 0, len(parts))
+	var currentSet []requirement
 	for i := range parts {
+		if len(parts[i]) == 0 || parts[i] == "||" || parts[i] == "-" || parts[i] == "*" {
+			continue
+		}
 		if i == 0 || parts[i-1] == "||" {
-			currentReq = make([]requirementFn, 0, len(parts)-i)
+			currentSet = make([]requirement, 0, len(parts)-i)
+		}
+		clean := stripPrefix(parts[i])
+		vLow, err := parseDown(clean)
+		if err != nil {
+			return nil, err
+		}
+		vHigh, round, err := parseUp(clean)
+		if err != nil {
+			return nil, err
+		}
+		var highComparator int
+		if round {
+			highComparator = svLT
+		} else {
+			highComparator = svLTE
 		}
 
-		switch parts[i][0] {
-		case '~':
-		case '^':
-		default:
-			if i < len(parts)-2 && parts[i+1] == "-" { //lower part of a hyphen range
-				v, err := parseDown(parts[i])
-				if err != nil {
-					return nil, err
-				}
-				currentReq = append(currentReq, v.GTE)
-			} else if i > 1 && parts[i-1] == "-" { //higher part of a hyphen range
-				v, round, err := parseUp(parts[i])
-				if err != nil {
-					return nil, err
-				}
-				if round {
-					currentReq = append(currentReq, v.LT)
+		if i < len(parts)-1 && parts[i+1] == "-" {
+			currentSet = append(currentSet, requirement{vLow, svGTE})
+		} else if i > 1 && parts[i-1] == "-" {
+			currentSet = append(currentSet, requirement{vHigh, highComparator})
+		} else {
+			switch parts[i][0] {
+			case '^':
+			case '~':
+			case '<':
+				if parts[i][1] == '=' {
+					currentSet = append(currentSet, requirement{vLow, svLTE})
 				} else {
-					currentReq = append(currentReq, v.LTE)
+					currentSet = append(currentSet, requirement{vLow, svLT})
 				}
-			} else { //exact or wildcard version
-				vHigh, round, err := parseUp(parts[i])
-				if err != nil {
-					return nil, err
+			case '>':
+				if parts[i][1] == '=' {
+					currentSet = append(currentSet, requirement{vLow, svGTE})
+				} else {
+					currentSet = append(currentSet, requirement{vLow, svGT})
 				}
-				if round {
-					currentReq = append(currentReq, vHigh.LT)
-				} else { //if not rounded, then we have an exact version
-					currentReq = append(currentReq, vHigh.EQ)
-					continue
-				}
-
-				vLow, err := parseDown(parts[i])
-				if err != nil {
-					return nil, err
-				}
-				currentReq = append(currentReq, v.GTE)
+			default:
+				currentSet = append(currentSet, requirement{vLow, svGTE})
+				currentSet = append(currentSet, requirement{vHigh, highComparator})
 			}
 		}
 
 		if i == len(parts)-1 || parts[i+1] == "||" {
-			sr.requirements = append(sr.requirements, currentReq)
+			sr.requirements = append(sr.requirements, currentSet)
 		}
 	}
 
-	requirements = strings.Replace(requirements, " || ", "||", -1)
-	requirements = strings.Replace(requirements, " - ", "--", -1)
-
-	ORs := strings.Split(requirements, "||")
-	for _, orv := range ORs {
-		ANDs := strings.Split(orv, " ")
-		for _, andv := range ANDs {
-
-		}
-	}
-
-	//split '||' sets (OR)
-	//split ' - ' ranges
-	//split by whitespace (AND)
-	//parse hyphen ranges
-	//parse X-Ranges
-	//parse Tilde Ranges
-	//parse Caret Ranges
-	return nil, nil
+	return sr, nil
 }
 
-func (s *SemverRequirements) Test(sv semver.Version) bool {
-	for _, v := range s.requirements {
-		if v.test(sv) {
+func (s *SemverRequirements) SatisfiedBy(sv semver.Version) bool {
+	if len(s.requirements) == 0 {
+		return true
+	}
+	for _, reqs := range s.requirements {
+		valid := true
+		for _, v := range reqs {
+			if !validPrerelease(v.version, sv) {
+				valid = false
+				break
+			}
+			if !checkVersion(v, sv) {
+				valid = false
+				break
+			}
+		}
+		if valid {
 			return true
 		}
 	}
 	return false
-}
-
-func (r requirementSet) test(sv semver.Version) bool {
-	for _, v := range r {
-		if !v(sv) {
-			return false
-		}
-	}
-	return true
 }
--- semver_test.go
+++ semver_test.go
@@ -5,6 +5,77 @@ import (
 	"testing"
 )
 
+func TestNewSemverRequirements(t *testing.T) {
+	check := func(req string, good []string, bad []string) {
+		svr, err := NewSemverRequirements(req)
+		if err != nil {
+			t.Fatalf("Failed to parse requirement string '%s': %s\n", req, err.Error())
+		}
+		for _, v := range good {
+			sv, err := semver.Parse(v)
+			if err != nil {
+				t.Fatalf("Bad test, failed to parse semver '%s': %s\n", v, err.Error())
+			}
+			if !svr.SatisfiedBy(sv) {
+				t.Errorf("Range '%s' rejected valid version '%s'", req, v)
+			}
+		}
+		for _, v := range bad {
+			sv, err := semver.Parse(v)
+			if err != nil {
+				t.Fatalf("Bad test, failed to parse semver '%s': %s\n", v, err.Error())
+			}
+			if svr.SatisfiedBy(sv) {
+				t.Errorf("Range '%s' accepted invalid version '%s'", req, v)
+			}
+		}
+
+	}
+
+	// examples from the node-semver readme
+	check(">=1.2.7", []string{"1.2.7", "1.2.8", "2.5.3", "1.3.9"}, []string{"1.2.6", "1.1.0"})
+	check(">=1.2.7 <1.3.0", []string{"1.2.7", "1.2.8", "1.2.99"}, []string{"1.2.6", "1.3.0", "1.1.0"})
+	check("1.2.7 || >=1.2.9 <2.0.0", []string{"1.2.7", "1.2.9", "1.4.6"}, []string{"1.2.8", "2.0.0"})
+	check(">1.2.3-alpha.3", []string{"1.2.3-alpha.7", "3.4.5"}, []string{"3.4.5-alpha.9"})
+
+	//hyphen ranges
+	check("1.2.3 - 2.3.4", []string{"1.2.3", "2.0.0", "2.3.4"}, []string{"1.2.2", "2.0.0-alpha", "2.3.5", "3.0.0", "2.4.0"})
+	check("1.2 - 2.3.4", []string{"1.2.0", "1.2.3", "2.0.0", "2.3.4"}, []string{"2.0.0-alpha", "2.3.5", "3.0.0"})
+	check("1.2 - 2.3", []string{"1.2.3", "1.2.0", "2.0.0", "2.3.4"}, []string{"2.4.0", "1.1.99", "3.0.0"})
+	check("1.2 - 2", []string{"1.2.0", "2.0.0", "2.99.99"}, []string{"3.0.0"})
+
+	//x-ranges
+	check("", []string{"0.0.0", "1.0.0", "1.0.0-alpha"}, []string{})
+	check("*", []string{"0.0.0", "1.0.0", "1.0.0-alpha"}, []string{})
+	check("1.x", []string{"1.0.0", "1.99.99"}, []string{"0.99.99", "1.2.0-alpha", "2.0.0"})
+	check("1.2.X", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+	check("1.2.*", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+	check("1", []string{"1.0.0", "1.99.99"}, []string{"0.99.99", "1.2.0-alpha", "2.0.0"})
+	check("1.2", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+
+	//tilde ranges
+	check("~1.2.3", []string{"1.2.3", "1.2.4", "1.2.99"}, []string{"1.3.0", "1.2.3-alpha"})
+	check("~1", []string{"1.0.0", "1.99.99"}, []string{"0.99.99", "1.2.0-alpha", "2.0.0"})
+	check("~1.2", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+	check("~0.2.3", []string{"0.2.3", "0.2.4"}, []string{"0.2.2", "0.3.0", "0.2.3-alpha"})
+	check("~0.2", []string{"0.2.0", "0.2.99"}, []string{"0.1.9", "0.3.0", "0.2.3-alpha"})
+	check("~0", []string{"0.0.0", "0.2.4"}, []string{"1.2.2", "1.0.0", "0.2.3-alpha"})
+	check("~1.2.3-beta.2", []string{"1.2.3-beta.2", "1.2.3", "1.2.4", "1.2.3-beta.3"}, []string{"1.2.2", "1.0.0", "1.2.4-beta.2", "1.3.0"})
+
+	//caret ranges
+	check("^1.2.3", []string{"1.2.3", "1.2.4", "1.9.9"}, []string{"1.2.4-alpha", "2.0.0"})
+	check("^0.2.3", []string{"0.2.3", "0.2.4", "0.2.9"}, []string{"1.2.4-alpha", "0.3.0", "2.0.0"})
+	check("^0.0.3", []string{"0.0.3"}, []string{"1.2.4-alpha", "0.3.0", "0.0.3-beta", "0.0.4", "2.0.0"})
+	check("^1.2.3-beta.2", []string{"1.2.3", "1.2.3-beta.3", "1.9.9", "1.2.3-beta.2"}, []string{"1.2.3-alpha", "1.2.2", "1.2.4-beta.2", "2.0.0"})
+	check("^0.0.3-beta", []string{"0.0.3", "0.0.3-pr.2", "0.0.3-beta"}, []string{"0.0.4", "0.0.3-alpha", "0.0.2"})
+	check("^1.2.x", []string{"1.2.0", "1.2.1", "1.99.99"}, []string{"1.1.2", "2.0.0", "1.2.0-beta"})
+	check("^0.0.x", []string{"0.0.0", "0.0.1", "0.0.99"}, []string{"1.1.2", "2.0.0", "1.2.0-beta", "0.0.0-beta", "0.1.0"})
+	check("^0.0", []string{"0.0.0", "0.0.1", "0.0.99"}, []string{"1.1.2", "2.0.0", "1.2.0-beta", "0.0.0-beta", "0.1.0"})
+	check("^1.x", []string{"1.0.0", "1.0.1", "1.99.5"}, []string{"2.0.0", "1.1.0-beta"})
+	check("^0.x", []string{"0.0.0", "0.1.0", "0.0.1", "0.1.1"}, []string{"1.0.0", "0.0.1-beta"})
+
+}
+
 func TestParseDown(t *testing.T) {
 	check := func(input, expected string) {
 		ex, err := semver.Parse(expected)
@@ -16,7 +87,7 @@ func TestParseDown(t *testing.T) {
 			t.Fatalf("Error parsing '%s': %s\n", input, err.Error())
 		}
 		if sv.NE(ex) {
-			t.Fatalf("Error got '%s' but expected '%s'\n", sv.String(), ex.String())
+			t.Errorf("Error got '%s' but expected '%s'\n", sv.String(), ex.String())
 		}
 	}
 
@@ -28,10 +99,13 @@ func TestParseDown(t *testing.T) {
 	check("", "0.0.0")
 	check("1", "1.0.0")
 	check("1.*", "1.0.0")
+	check("0.2", "0.2.0")
+	check("0", "0.0.0")
+	check("1.2.3-beta.2", "1.2.3-beta.2")
 	check("1.0.0-rc.1", "1.0.0-rc.1")
 	check("1.0.0-rc", "1.0.0-rc")
-	check("1.0.0-rc+foo", "1.0.0-rc+foo")
-	check("1.0.0+foo", "1.0.0+foo")
+	check("1.0.0-rc+foo", "1.0.0-rc")
+	check("1.0.0+foo", "1.0.0")
 }
 func TestParseUp(t *testing.T) {
 	check := func(input, expected string, rounded bool) {
@@ -44,13 +118,17 @@ func TestParseUp(t *testing.T) {
 			t.Fatalf("Error parsing '%s': %s\n", input, err.Error())
 		}
 		if sv.NE(ex) {
-			t.Fatalf("Error, got '%s' but expected '%s'\n", sv.String(), ex.String())
+			t.Errorf("Error, got '%s' but expected '%s'\n", sv.String(), ex.String())
 		}
 		if r != rounded {
-			t.Fatalf("Error, rounding reported '%b' but expected '%b'\n", r, rounded)
+			t.Errorf("Error, rounding reported '%b' but expected '%b'\n", r, rounded)
 		}
 	}
 
+	check("2.3.4", "2.3.4", false)
+	check("2.3", "2.4.0", true)
+	check("2", "3.0.0", true)
+	check("1.x", "2.0.0", true)
 	check("1.2.3", "1.2.3", false)
 	check("1.2", "1.3.0", true)
 	check("1.x", "2.0.0", true)
@@ -59,6 +137,6 @@ func TestParseUp(t *testing.T) {
 	check("1.*", "2.0.0", true)
 	check("1.0.0-rc.1", "1.0.0-rc.1", false)
 	check("1.0.0-rc", "1.0.0-rc", false)
-	check("1.0.0-rc+foo", "1.0.0-rc+foo", false)
-	check("1.0.0+foo", "1.0.0+foo", false)
+	check("1.0.0-rc+foo", "1.0.0-rc", false)
+	check("1.0.0+foo", "1.0.0", false)
 }
