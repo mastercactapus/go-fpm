9b24217711207d65c154b2dc6e929fb85527da7a
Nathan Caza <mastercactapus@gmail.com>
2015-05-18 15:24:47 +0000

    move semver range to it's own package

---
+++ semver/semver.go
@@ -0,0 +1,286 @@
+package semver
+
+import (
+	"github.com/blang/semver"
+	"strconv"
+	"strings"
+)
+
+const (
+	svLT = iota
+	svLTE
+	svGT
+	svGTE
+	svEQ
+)
+
+
+type requirement struct {
+	version semver.Version
+	svType  int
+}
+
+type SemverRequirements struct {
+	requirements [][]requirement
+}
+
+var cleanup = strings.NewReplacer("  ", " ", "> ", ">", "= ", "=", "< ", "<")
+
+//strips any prefix for the version for parsing
+func stripPrefix(version string) string {
+	switch version[0] {
+	case '>', '<':
+		if version[1] == '=' {
+			version = version[2:]
+		} else {
+			version = version[1:]
+		}
+	case '=', '^', '~':
+		version = version[1:]
+	}
+
+	if version[0] == 'v' {
+		version = version[1:]
+	}
+
+	return version
+}
+
+//parses, replacing missing/wildcards with zero
+func parseDown(version string) (sv semver.Version, err error) {
+	if len(version) == 0 {
+		//return version 0.0.0
+		return
+	}
+	parts := strings.SplitN(version, ".", 3)
+	if len(parts) == 1 {
+		if parts[0] == "x" || parts[0] == "X" || parts[0] == "*" {
+			parts[0] = "0"
+		}
+		return semver.Parse(parts[0] + ".0.0")
+	}
+	if len(parts) == 2 {
+		if parts[1] == "x" || parts[1] == "X" || parts[1] == "*" {
+			parts[1] = "0"
+		}
+		return semver.Parse(parts[0] + "." + parts[1] + ".0")
+	}
+	if parts[2] == "x" || parts[2] == "X" || parts[2] == "*" {
+		parts[2] = "0"
+	}
+	version = strings.Join(parts, ".")
+	buildIndex := strings.IndexRune(version, '+')
+	if buildIndex != -1 {
+		version = version[:buildIndex]
+	}
+	return semver.Parse(version)
+}
+
+//parses, replacing missing/wildcards by incrementing the higher version
+func parseUp(version string) (sv semver.Version, round bool, err error) {
+	parts := strings.SplitN(version, ".", 3)
+	var major, minor int64
+
+	major, err = strconv.ParseInt(parts[0], 10, 64)
+	if err != nil {
+		return
+	}
+	if len(parts) == 1 || parts[1] == "x" || parts[1] == "X" || parts[1] == "*" {
+		major++
+		round = true
+		sv, err = semver.Parse(strconv.Itoa(int(major)) + ".0.0")
+		return
+	}
+
+	minor, err = strconv.ParseInt(parts[1], 10, 64)
+	if err != nil {
+		return
+	}
+	if len(parts) == 2 || parts[2] == "x" || parts[2] == "X" || parts[2] == "*" {
+		minor++
+		round = true
+		sv, err = semver.Parse(strconv.Itoa(int(major)) + "." + strconv.Itoa(int(minor)) + ".0")
+		return
+	}
+
+	sv, err = semver.Parse(version)
+	return
+}
+
+//checks, if a prerelease, that the major,minor,patch tuple match
+func validPrerelease(orig semver.Version, sv semver.Version) bool {
+	//test version has no prerelease tag, then it's fine
+	if sv.Pre == nil || len(sv.Pre) == 0 {
+		return true
+	}
+
+	//in this case it DOES have a pre-tag
+	//but if the original does NOT then it's invalid
+	if orig.Pre == nil && len(orig.Pre) == 0 {
+		return false
+	}
+
+	//otherwise check that the tuple is matching
+	return orig.Major == sv.Major && orig.Minor == sv.Minor && orig.Patch == sv.Patch
+}
+
+func checkVersion(req requirement, sv semver.Version) bool {
+	switch req.svType {
+	case svLT:
+		return sv.LT(req.version)
+	case svLTE:
+		return sv.LTE(req.version)
+	case svGT:
+		return sv.GT(req.version)
+	case svGTE:
+		return sv.GTE(req.version)
+	case svEQ:
+		return sv.EQ(req.version)
+	default:
+		panic("Unknown comparator")
+	}
+}
+
+func (r *requirement) String() string {
+	switch r.svType {
+	case svLT:
+		return "<" + r.version.String()
+	case svLTE:
+		return "<=" + r.version.String()
+	case svGT:
+		return ">" + r.version.String()
+	case svGTE:
+		return ">=" + r.version.String()
+	case svEQ:
+		return "=" + r.version.String()
+	default:
+		panic("Unknown `svType`")
+	}
+}
+
+func (s *SemverRequirements) String() string {
+	ors := make([]string, 0, len(s.requirements))
+	for _, req := range s.requirements {
+		ands := make([]string, 0, len(req))
+		for _, v := range req {
+			ands = append(ands, v.String())
+		}
+		ors = append(ors, strings.Join(ands, " "))
+	}
+	return strings.Join(ors, " || ")
+}
+
+//parses a requirements string using the format from here: https://github.com/npm/node-semver
+func NewSemverRequirements(requirements string) (*SemverRequirements, error) {
+	sr := new(SemverRequirements)
+
+	//cleanup, trim and remove duplicate whitespace
+	requirements = cleanup.Replace(strings.TrimSpace(requirements))
+	parts := strings.Split(requirements, " ")
+	sr.requirements = make([][]requirement, 0, len(parts))
+	var currentSet []requirement
+	for i := range parts {
+		if len(parts[i]) == 0 || parts[i] == "||" || parts[i] == "-" || parts[i] == "*" {
+			continue
+		}
+		if i == 0 || parts[i-1] == "||" {
+			currentSet = make([]requirement, 0, len(parts)-i)
+		}
+		clean := stripPrefix(parts[i])
+		vLow, err := parseDown(clean)
+		if err != nil {
+			return nil, err
+		}
+		vHigh, round, err := parseUp(clean)
+		if err != nil {
+			return nil, err
+		}
+		var highComparator int
+		if round {
+			highComparator = svLT
+		} else {
+			highComparator = svLTE
+		}
+
+		if i < len(parts)-1 && parts[i+1] == "-" {
+			currentSet = append(currentSet, requirement{vLow, svGTE})
+		} else if i > 1 && parts[i-1] == "-" {
+			currentSet = append(currentSet, requirement{vHigh, highComparator})
+		} else {
+			switch parts[i][0] {
+			case '^':
+				currentSet = append(currentSet, requirement{vLow, svGTE})
+				if vLow.Major != 0 {
+					vHigh, _, err = parseUp(strconv.Itoa(int(vLow.Major)) + ".x")
+					if err != nil {
+						return nil, err
+					}
+					currentSet = append(currentSet, requirement{vHigh, svLT})
+				} else if vLow.Minor != 0 {
+					vHigh, _, err = parseUp("0." + strconv.Itoa(int(vLow.Minor)) + ".x")
+					if err != nil {
+						return nil, err
+					}
+					currentSet = append(currentSet, requirement{vHigh, svLT})
+				} else {
+					vHigh.Pre = nil
+					currentSet = append(currentSet, requirement{vHigh, highComparator})
+				}
+			case '~':
+				currentSet = append(currentSet, requirement{vLow, svGTE})
+				vparts := strings.SplitN(clean, ".", 2)
+				if len(vparts) == 1 {
+					vHigh, _, err = parseUp(strconv.Itoa(int(vLow.Major)))
+				} else {
+					vHigh, _, err = parseUp(strconv.Itoa(int(vLow.Major)) + "." + strconv.Itoa(int(vLow.Minor)))
+				}
+				currentSet = append(currentSet, requirement{vHigh, svLT})
+			case '<':
+				if parts[i][1] == '=' {
+					currentSet = append(currentSet, requirement{vLow, svLTE})
+				} else {
+					currentSet = append(currentSet, requirement{vLow, svLT})
+				}
+			case '>':
+				if parts[i][1] == '=' {
+					currentSet = append(currentSet, requirement{vLow, svGTE})
+				} else {
+					currentSet = append(currentSet, requirement{vLow, svGT})
+				}
+			default:
+				currentSet = append(currentSet, requirement{vLow, svGTE})
+				currentSet = append(currentSet, requirement{vHigh, highComparator})
+			}
+		}
+
+		if i == len(parts)-1 || parts[i+1] == "||" {
+			sr.requirements = append(sr.requirements, currentSet)
+		}
+	}
+
+	return sr, nil
+}
+
+//Checks if a semver version satisfies the requirements or not
+func (s *SemverRequirements) SatisfiedBy(sv semver.Version) bool {
+	if len(s.requirements) == 0 {
+		return true
+	}
+	for _, reqs := range s.requirements {
+		valid := true
+		for _, v := range reqs {
+			if (v.svType == svGT || v.svType == svGTE) && !validPrerelease(v.version, sv) {
+				valid = false
+				break
+			}
+			if !checkVersion(v, sv) {
+				valid = false
+				break
+			}
+		}
+		if valid {
+			return true
+		}
+	}
+	return false
+}
---
+++ semver/semver_test.go
@@ -0,0 +1,163 @@
+package semver
+
+import (
+	"github.com/blang/semver"
+	"testing"
+)
+
+func _semverReqCheck(t *testing.T) func(string, []string, []string) {
+	return func(req string, good []string, bad []string) {
+		svr, err := NewSemverRequirements(req)
+		if err != nil {
+			t.Fatalf("Failed to parse requirement string '%s': %s\n", req, err.Error())
+		}
+
+		t.Logf("Interpereted '%s' as '%s'\n", req, svr.String())
+		for _, v := range good {
+			sv, err := semver.Parse(v)
+			if err != nil {
+				t.Fatalf("Bad test, failed to parse semver '%s': %s\n", v, err.Error())
+			}
+			if !svr.SatisfiedBy(sv) {
+				t.Errorf("Range '%s' rejected valid version '%s'", req, v)
+			}
+		}
+		for _, v := range bad {
+			sv, err := semver.Parse(v)
+			if err != nil {
+				t.Fatalf("Bad test, failed to parse semver '%s': %s\n", v, err.Error())
+			}
+			if svr.SatisfiedBy(sv) {
+				t.Errorf("Range '%s' accepted invalid version '%s'", req, v)
+			}
+		}
+	}
+}
+
+func TestNewSemverRequirements_direct(t *testing.T) {
+	check := _semverReqCheck(t)
+
+	// examples from the node-semver readme
+	check(">=1.2.7", []string{"1.2.7", "1.2.8", "2.5.3", "1.3.9"}, []string{"1.2.6", "1.1.0"})
+	check(">=1.2.7 <1.3.0", []string{"1.2.7", "1.2.8", "1.2.99"}, []string{"1.2.6", "1.3.0", "1.1.0"})
+	check("1.2.7 || >=1.2.9 <2.0.0", []string{"1.2.7", "1.2.9", "1.4.6"}, []string{"1.2.8", "2.0.0"})
+	check(">1.2.3-alpha.3", []string{"1.2.3-alpha.7", "3.4.5"}, []string{"3.4.5-alpha.9"})
+	check(">= 0.3.0", []string{"0.3.0", "0.3.1", "1.0.0"}, []string{"0.0.0", "0.2.0", "0.2.9", "0.3.0-beta"})
+}
+
+func TestNewSemverRequirements_hyphens(t *testing.T) {
+	check := _semverReqCheck(t)
+
+	//hyphen ranges
+	check("1.2.3 - 2.3.4", []string{"1.2.3", "2.0.0", "2.3.4"}, []string{"1.2.2", "2.0.0-alpha", "2.3.5", "3.0.0", "2.4.0"})
+	check("1.2 - 2.3.4", []string{"1.2.0", "1.2.3", "2.0.0", "2.3.4"}, []string{"2.0.0-alpha", "2.3.5", "3.0.0"})
+	check("1.2 - 2.3", []string{"1.2.3", "1.2.0", "2.0.0", "2.3.4"}, []string{"2.4.0", "1.1.99", "3.0.0"})
+	check("1.2 - 2", []string{"1.2.0", "2.0.0", "2.99.99"}, []string{"3.0.0"})
+}
+
+func TestNewSemverRequirements_xranges(t *testing.T) {
+	check := _semverReqCheck(t)
+
+	//x-ranges
+	check("", []string{"0.0.0", "1.0.0", "1.0.0-alpha"}, []string{})
+	check("*", []string{"0.0.0", "1.0.0", "1.0.0-alpha"}, []string{})
+	check("1.x", []string{"1.0.0", "1.99.99"}, []string{"0.99.99", "1.2.0-alpha", "2.0.0"})
+	check("1.2.X", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+	check("1.2.*", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+	check("1", []string{"1.0.0", "1.99.99"}, []string{"0.99.99", "1.2.0-alpha", "2.0.0"})
+	check("1.2", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+}
+
+func TestNewSemverRequirements_caret(t *testing.T) {
+	check := _semverReqCheck(t)
+
+	//caret ranges
+	check("^1.2.3", []string{"1.2.3", "1.2.4", "1.9.9"}, []string{"1.2.4-alpha", "2.0.0"})
+	check("^0.2.3", []string{"0.2.3", "0.2.4", "0.2.9"}, []string{"1.2.4-alpha", "0.3.0", "2.0.0"})
+	check("^0.0.3", []string{"0.0.3"}, []string{"1.2.4-alpha", "0.3.0", "0.0.3-beta", "0.0.4", "2.0.0"})
+	check("^1.2.3-beta.2", []string{"1.2.3", "1.2.3-beta.3", "1.9.9", "1.2.3-beta.2"}, []string{"1.2.3-alpha", "1.2.2", "1.2.4-beta.2", "2.0.0"})
+	check("^0.0.3-beta", []string{"0.0.3", "0.0.3-pr.2", "0.0.3-beta"}, []string{"0.0.4", "0.0.3-alpha", "0.0.2"})
+	check("^1.2.x", []string{"1.2.0", "1.2.1", "1.99.99"}, []string{"1.1.2", "2.0.0", "1.2.0-beta"})
+	check("^0.0.x", []string{"0.0.0", "0.0.1", "0.0.99"}, []string{"1.1.2", "2.0.0", "1.2.0-beta", "0.0.0-beta", "0.1.0"})
+	check("^0.0", []string{"0.0.0", "0.0.1", "0.0.99"}, []string{"1.1.2", "2.0.0", "1.2.0-beta", "0.0.0-beta", "0.1.0"})
+	check("^1.x", []string{"1.0.0", "1.0.1", "1.99.5"}, []string{"2.0.0", "1.1.0-beta"})
+	check("^0.x", []string{"0.0.0", "0.1.0", "0.0.1", "0.1.1"}, []string{"1.0.0", "0.0.1-beta"})
+}
+
+func TestNewSemverRequirements_tilde(t *testing.T) {
+	check := _semverReqCheck(t)
+
+	//tilde ranges
+	check("~1.2.3", []string{"1.2.3", "1.2.4", "1.2.99"}, []string{"1.3.0", "1.2.3-alpha"})
+	check("~1", []string{"1.0.0", "1.99.99"}, []string{"0.99.99", "1.2.0-alpha", "2.0.0"})
+	check("~1.2", []string{"1.2.0", "1.2.99"}, []string{"0.99.99", "1.2.1-alpha", "2.0.0", "1.3.0"})
+	check("~0.2.3", []string{"0.2.3", "0.2.4"}, []string{"0.2.2", "0.3.0", "0.2.3-alpha"})
+	check("~0.2", []string{"0.2.0", "0.2.99"}, []string{"0.1.9", "0.3.0", "0.2.3-alpha"})
+	check("~0", []string{"0.0.0", "0.2.4"}, []string{"1.2.2", "1.0.0", "0.2.3-alpha"})
+	check("~1.2.3-beta.2", []string{"1.2.3-beta.2", "1.2.3", "1.2.4", "1.2.3-beta.3"}, []string{"1.2.2", "1.0.0", "1.2.4-beta.2", "1.3.0"})
+}
+
+func TestParseDown(t *testing.T) {
+	check := func(input, expected string) {
+		ex, err := semver.Parse(expected)
+		if err != nil {
+			t.Fatalf("Test error parsing expected '%s': %s\n", expected, err.Error())
+		}
+		sv, err := parseDown(input)
+		if err != nil {
+			t.Fatalf("Error parsing '%s': %s\n", input, err.Error())
+		}
+		if sv.NE(ex) {
+			t.Errorf("Error got '%s' but expected '%s'\n", sv.String(), ex.String())
+		}
+	}
+
+	check("1.2.3", "1.2.3")
+	check("1.2", "1.2.0")
+	check("*", "0.0.0")
+	check("1.x", "1.0.0")
+	check("1.2.X", "1.2.0")
+	check("", "0.0.0")
+	check("1", "1.0.0")
+	check("1.*", "1.0.0")
+	check("0.2", "0.2.0")
+	check("0", "0.0.0")
+	check("1.2.3-beta.2", "1.2.3-beta.2")
+	check("1.0.0-rc.1", "1.0.0-rc.1")
+	check("1.0.0-rc", "1.0.0-rc")
+	check("1.0.0-rc+foo", "1.0.0-rc")
+	check("1.0.0+foo", "1.0.0")
+}
+func TestParseUp(t *testing.T) {
+	check := func(input, expected string, rounded bool) {
+		ex, err := semver.Parse(expected)
+		if err != nil {
+			t.Fatalf("Test error parsing expected '%s': %s\n", expected, err.Error())
+		}
+		sv, r, err := parseUp(input)
+		if err != nil {
+			t.Fatalf("Error parsing '%s': %s\n", input, err.Error())
+		}
+		if sv.NE(ex) {
+			t.Errorf("Error, got '%s' but expected '%s'\n", sv.String(), ex.String())
+		}
+		if r != rounded {
+			t.Errorf("Error, rounding reported '%b' but expected '%b'\n", r, rounded)
+		}
+	}
+
+	check("2.3.4", "2.3.4", false)
+	check("2.3", "2.4.0", true)
+	check("2", "3.0.0", true)
+	check("1.x", "2.0.0", true)
+	check("1.2.3", "1.2.3", false)
+	check("1.2", "1.3.0", true)
+	check("1.x", "2.0.0", true)
+	check("1.2.X", "1.3.0", true)
+	check("1", "2.0.0", true)
+	check("1.*", "2.0.0", true)
+	check("1.0.0-rc.1", "1.0.0-rc.1", false)
+	check("1.0.0-rc", "1.0.0-rc", false)
+	check("1.0.0-rc+foo", "1.0.0-rc", false)
+	check("1.0.0+foo", "1.0.0", false)
+}
