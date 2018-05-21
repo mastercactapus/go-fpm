dc1123bbd69c51c8a01831cd8b67861964df924b
Nathaniel Caza <mastercactapus@gmail.com>
2018-05-21 08:14:54 -0500

    add tests around equality operator

--- semver_test.go
+++ semver_test.go
@@ -44,6 +44,7 @@ func TestNewSemverRequirements_direct(t *testing.T) {
 	check("1.2.7 || >=1.2.9 <2.0.0", []string{"1.2.7", "1.2.9", "1.4.6"}, []string{"1.2.8", "2.0.0"})
 	check(">1.2.3-alpha.3", []string{"1.2.3-alpha.7", "3.4.5"}, []string{"3.4.5-alpha.9"})
 	check(">= 0.3.0", []string{"0.3.0", "0.3.1", "1.0.0"}, []string{"0.0.0", "0.2.0", "0.2.9", "0.3.0-beta"})
+	check("1.2.7 || =1.2.8", []string{"1.2.7", "1.2.8"}, []string{"1.2.9", "1.2.6", "2.0.0"})
 }
 
 func TestNewSemverRequirements_hyphens(t *testing.T) {
@@ -116,6 +117,8 @@ func TestNewSemverRequirements_invalid(t *testing.T) {
 	check("💩")
 	check("💩💩")
 	check("=<")
+	check("==")
+	check("==1.2.3")
 }
 
 func TestParseDown(t *testing.T) {
